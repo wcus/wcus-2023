723727a912ef5682a7f13c430a2059d9d0beec9e
Kelly Dwan <ryelle@users.noreply.github.com>
2023-05-16 15:00:18 -0400

    Remove `postcss-short` from config
    This plugin was stripping the `!important` from `font-size` properties, but it's not necessary as there are no shorthand properties in the CSS.

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
 	plugins: [
 		require( 'postcss-import' ),
 		require( 'postcss-custom-properties' )( { preserve: false } ),
-		require( 'postcss-short' ),
 		require( 'postcss-nesting' ),
 		require( 'autoprefixer' ),
 	],
